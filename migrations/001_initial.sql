881972975c2df67c197242ab131113b65f7cdfce
RayanZaki <rayan.hassici@ensia.edu.dz>
2025-07-18 16:28:51 +0100

    fixed typo

--- migrations/001_initial.sql
+++ migrations/001_initial.sql
@@ -35,4 +35,3 @@ CREATE INDEX idx_contacts_area ON contacts(min_area_sqm, max_area_sqm);
 CREATE INDEX idx_contacts_rooms ON contacts(min_rooms);
 CREATE INDEX idx_contacts_preferred_locations_gin ON contacts USING GIN(preferred_locations);
 CREATE INDEX idx_contacts_property_types_gin ON contacts USING GIN(property_types);
-q
